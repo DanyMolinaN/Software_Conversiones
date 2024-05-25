63122af7941854854e6282f171e398128a060bbc
LeninReyesEPN <lenin.reyes@epn.edu.ec>
2024-05-24 23:05:13 -0600

    Conexion a base de datos MySQL

--- src/Main.java
+++ src/Main.java
@@ -4,51 +4,53 @@ import java.sql.*;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws SQLException {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
 
-        System.out.println("Primer cambio LeninReyesEPN");
-
+        //variables para la conexion
+        Connection conn = null;
         Statement stmt = null;
 
-        try {
-            //conexion a la base de datos
-            Connection obj = DriverManager.getConnection("jdbc:mysql://localhost:3306/boolean_binary_master","root","basededatos");
-            System.out.println("conexion exitosa "+obj);
+        /*Datos para la coneccion */
+        String base="boolean_binary_master";
+        String url= "jdbc:mysql://localhost:3306/"+base;
+        String user="root";
+        String password="basededatos";
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn=DriverManager.getConnection(url,user,password);
+            System.out.println("conexion exitosa");
 
-            //Empleando la busquedad
-            System.out.println("Creando la declaración...\n");
-            stmt = obj.createStatement();
+            //script de la consulta
+            System.out.println("Creando la declaración...");
+            stmt = conn.createStatement();
             String sql;
             sql = "SELECT * FROM usuarios";
             ResultSet rs = stmt.executeQuery(sql);
 
-            //Extraer datos del conjunto de resultados
+            //extraer datos del conjunto de resultados
             while (rs.next()) {
                 // Recuperar por columna
                 int id = rs.getInt("id");
                 String nombre = rs.getString("nombre");
                 String apellido = rs.getString("apellido");
-                String correo = rs.getString("correo");
                 String telefono = rs.getString("telefono");
                 String usuario = rs.getString("usuario");
+                String correo = rs.getString("correo");
                 // Imprimir los valores
                 System.out.print("ID: " + id);
                 System.out.print(", Nombre: " + nombre);
                 System.out.print(", Apellido: " + apellido);
-                System.out.print(", Correo: " + correo);
                 System.out.print(", Telefono: " + telefono);
                 System.out.print(", Usuario: " + usuario);
+                System.out.print(", Correo: " + correo);
                 System.out.println();
             }
             //cerrar recursos
             rs.close();
             stmt.close();
-            obj.close();
+            conn.close();
 
-        }catch (Exception e){
-            System.out.println(e);
+        }catch (ClassNotFoundException | SQLException e){
+            System.err.println(e);
         }
-
     }
 }
